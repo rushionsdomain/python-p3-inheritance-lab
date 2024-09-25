e509b4141d57e5ba71f0a2cf101b1031ae499d5e
Gachuri <rushionchegge@gmail.com>
2024-09-25 10:57:10 +0300

    Implemented User, Student, and Teacher classes to pass all tests

--- lib/teacher.py
+++ lib/teacher.py
@@ -1,10 +1,9 @@
-#!/usr/bin/env python
-
 from user import User
 
-import random
-
 class Teacher(User):
+    def __init__(self, first_name, last_name):
+        super().__init__(first_name, last_name)
+        self.knowledge = ["Mathematics", "Science", "History"]  # Example knowledge base
 
     def teach(self):
-        pass
+        return self.knowledge[0]  # Example method to return a subject taught
